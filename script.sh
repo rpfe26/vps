0ff0ad58eaa5bb2283d24e031381987f0db1b813
rpfe26 <59179452+rpfe26@users.noreply.github.com>
2019-12-23 20:27:26 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# installation automatisée VPS 
+
+apt-get update 
+apt-get upgrade 
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+apt update
+
+docker run hello-world
