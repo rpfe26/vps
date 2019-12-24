462a36ec8444afeb0e968f36826b1b50f21ce198
rpfe26 <59179452+rpfe26@users.noreply.github.com>
2019-12-24 11:01:17 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,24 +2,23 @@
 
 # installation automatisée VPS 
 
-apt-get update 
-apt-get upgrade 
+apt-get update && apt-get upgrade 
 
-apt-get install \
+
+sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg2 \
-    software-properties-common
-
+    software-properties-common -y
+    
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+apt-get update
 
-apt update
-apt autoremove -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
 
-docker run hello-world
+curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
