6170c9efdfca719acc035626a8add9ea432a92ea
rpfe26 <59179452+rpfe26@users.noreply.github.com>
2019-12-24 11:07:58 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,6 +14,11 @@ sudo apt-get install \
     
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
 apt-get update
 
 apt-get install docker-ce docker-ce-cli containerd.io -y
